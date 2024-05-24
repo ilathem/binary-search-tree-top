bff05f7f9d6124e6bc9a940442a2e009979d06c4
Isaiah Lathem <isaiahlathem@gmail.com>
2024-05-23 21:53:30 -0500

    implement buildTree

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
+import mergeSort from "./mergeSort.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+
 class Node {
-  constructor(data = null, left = null, right = null) {
+  constructor(data) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
+  }
+
+  toString() {
+    return this.data;
   }
 }
 
 class Tree {
   constructor(data = []) {
-    this.root = this.buildTree(data);
+    const end = data.length - 1
+    this.root = this.buildTree(mergeSort(data), 0, end);
   }
-  
-  buildTree(data = []) {
 
+  buildTree(data, start, end) {
+    if (start > end) return null;
+    const mid = parseInt((start + end) / 2);
+    const node = new Node(data[mid]);
+    node.left = this.buildTree(data, start, mid - 1);
+    node.right = this.buildTree(data, mid + 1, end);
+    return node;
   }
+
 }
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
